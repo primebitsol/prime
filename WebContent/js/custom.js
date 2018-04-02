73ad87cb353cb7fa5a904911c101103fc987db4b
shravani-radhakrishnan <manduvasravani@gmail.com>
2018-04-02 04:55:19 -0700

    tenth commit

--- WebContent/js/custom.js
+++ WebContent/js/custom.js
@@ -27,20 +27,29 @@ jQuery(function($){
 /* ----------------------------------------------------------- */
   /*  1. Login MENU
   /* ----------------------------------------------------------- */
-
+	 
+	    $(document).ready(function(){
+		    $('.carousel .vertical .item').each(function(){
+		  var next = $(this).next();
+		  if (!next.length) {
+		    next = $(this).siblings(':first');
+		  }
+		  next.children(':first-child').clone().appendTo($(this));
+		  
+		  for (var i=1;i<2;i++) {
+		    next=next.next();
+		    if (!next.length) {
+		        next = $(this).siblings(':first');
+		    }
+		    
+		    next.children(':first-child').clone().appendTo($(this));
+		  }
+		});
+	    });
+	   
 	
 	
-	/* ----------------------------------------------------------- */
-  /*  1. DROPDOWN MENU
-  /* ----------------------------------------------------------- */
-
-   // for hover dropdown menu
-  /*$('ul.nav li.dropdown').hover(function() {
-      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
-    }, function() {
-      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
-    });
-	*/
+	
 	/* ----------------------------------------------------------- */
 	/*  2. SUPERSLIDES SLIDER
 	/* ----------------------------------------------------------- */
