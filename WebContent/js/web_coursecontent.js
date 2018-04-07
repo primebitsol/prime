c0d5c2d8a21bf921568eca8954f41fa26056abc6
shravani-radhakrishnan <manduvasravani@gmail.com>
2018-04-06 17:09:31 -0700

    Fourteenth commit

--- WebContent/js/web_coursecontent.js
+++ WebContent/js/web_coursecontent.js
@@ -3,7 +3,8 @@
 //script for list view and grid view
     $(document).ready(function() {
     $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
-    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');
+    $('#products .item').addClass('grid-group-item');});
 });
 
 //script for filtering the course
